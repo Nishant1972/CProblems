3aa80f7a10ad6301c38b54469c558049ba2d9b34
Harshit Jain <114314853+Jainharshit09@users.noreply.github.com>
2022-10-31 18:52:19 +0530

    Update 2148. Count Elements With Strictly Smaller and Greater Elements.cpp

--- Leetcode/2148. Count Elements With Strictly Smaller and Greater Elements.cpp
+++ Leetcode/2148. Count Elements With Strictly Smaller and Greater Elements.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int countElements(vector<int>& nums) {
-        int mn = *min_element(nums.begin(), nums.end());
-        int mx = *max_element(nums.begin(), nums.end());
-        int count = 0;
+        int mn = nums[0], mx = nums[0];
+        for(int i = 0; i < nums.size(); ++i) {
+            mn = min(mn, nums[i]);
+            mx = max(mx, nums[i]);
+        }
         
+        int count = 0;    
         for(auto x : nums) {
             if(x != mn && x != mx)
                 ++count;
